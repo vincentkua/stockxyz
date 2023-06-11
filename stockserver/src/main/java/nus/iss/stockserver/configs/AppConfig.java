0a9c5cd2f0f928ff45cfc0fd6af3e676b10327ec
atlaskua <zylyn89@gmail.com>
2023-06-11 23:21:15 +0800

    railway server connection tested

--- stockserver/src/main/java/nus/iss/stockserver/configs/AppConfig.java
+++ stockserver/src/main/java/nus/iss/stockserver/configs/AppConfig.java
@@ -1,11 +1,26 @@
 package nus.iss.stockserver.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
 @Configuration
 public class AppConfig {
+
+    @Value("${mongo.url}")
+    private String mongoUrl;
+
+    @Bean
+    public MongoTemplate createMongoTemplate() {
+        MongoClient client = MongoClients.create(mongoUrl);
+        return new MongoTemplate(client, "stockdb");
+    }
 
     @Bean
     public WebMvcConfigurer configureCors() {
