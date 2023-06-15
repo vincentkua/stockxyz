80a9b9c392c460cbf36213d9387b1cb4a2929234
atlaskua <zylyn89@gmail.com>
2023-06-15 23:43:31 +0800

    added spring security

---
+++ stockserver/src/main/java/nus/iss/stockserver/configs/SecurityConfig.java
@@ -0,0 +1,22 @@
+package nus.iss.stockserver.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    
+    @Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/api/**","/").permitAll()  //allow all access from /api
+				.anyRequest().authenticated()
+			);
+
+		return http.build();
+	}
+    
+}
